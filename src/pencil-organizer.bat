b6f7412c8e045635f24e96726944c354bf0cba3e
Jostein Skaar <jostein.skaar@gmail.com>
2024-01-10 21:25:30 +0100

    feat: add experiment with pencil organizer

--- src/pencil-organizer.bat
+++ src/pencil-organizer.bat
@@ -1,8 +1,22 @@
-openscad -D "width=8;length=12;height=3;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-8x12h3-bambu.stl pencil-organizer.scad
-openscad -D "width=8;length=12;height=6;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-8x12h6-bambu.stl pencil-organizer.scad
-openscad -D "width=8;length=8;height=3;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-8x8h3-bambu.stl pencil-organizer.scad
-openscad -D "width=4;length=8;height=9;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x8h9-bambu.stl pencil-organizer.scad
-openscad -D "width=4;length=4;height=12;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x4h12-bambu.stl pencil-organizer.scad
-openscad -D "width=4;length=4;height=3;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x4h3-bambu.stl pencil-organizer.scad
-openscad -D "width=4;length=4;height=6;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x4h6-bambu.stl pencil-organizer.scad
-openscad -D "width=4;length=4;height=9;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x4h9-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=4;length=4;height=4;use_inside_walls=false;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x4h4-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=4;length=4;height=4;use_inside_walls=true;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x4h4walls-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=4;length=4;height=6;use_inside_walls=false;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x4h6-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=4;length=4;height=6;use_inside_walls=true;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x4h6walls-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=4;length=8;height=2;use_inside_walls=true;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x8h2walls-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=4;length=8;height=1;use_inside_walls=false;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x8h1-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=4;length=4;height=6;use_inside_walls=true;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-4x4h6walls-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=2;height=4;use_inside_walls=true;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x2h4walls-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=2;height=6;use_inside_walls=true;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x2h6walls-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=6;height=2;use_inside_walls=false;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x6h2-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=2;height=2;use_inside_walls=false;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x2h2-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=2;height=1;use_inside_walls=false;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x2h1-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=2;height=4;use_inside_walls=true;x_walls=1;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x2h4walls1-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=2;height=6;use_inside_walls=true;x_walls=1;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x2h6walls1-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=6;height=4;use_inside_walls=true;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x6h4walls-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=6;height=6;use_inside_walls=true;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x6h6walls-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=4;height=6;use_inside_walls=true;x_walls=1;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x4h6walls1-bambu.stl pencil-organizer.scad
+
+@REM openscad -D "width=6;length=4;height=6;use_inside_walls=true;x_walls=1;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x4h6walls1-bambu.stl pencil-organizer.scad
+@REM openscad -D "width=6;length=2;height=4;use_inside_walls=true;x_walls=1;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x2h4walls1-bambu.stl pencil-organizer.scad
+openscad -D "width=6;length=2;height=4;use_inside_walls=true;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x2h4walls-bambu.stl pencil-organizer.scad
+openscad -D "width=6;length=4;height=2;use_inside_walls=false;printer=""bambu""" -o ../stl/pencil-organizer/pencil-organizer-6x4h2-bambu.stl pencil-organizer.scad
--- src/pencil-organizer.scad
+++ src/pencil-organizer.scad
@@ -1,5 +1,5 @@
 // clang-format off
-include<bricklib.scad>;
+include<bigbricklib.scad>;
 include<brick-printer-adjustments.scad>;
 // clang-format on
 
@@ -10,21 +10,39 @@ echo("$brick_printer_adjustments", printer, $brick_printer_adjustments);
 
 width = 4;
 length = 4;
-height = 3; // 3,6,9,12
+height = 2; // 1,2,4,6
+tube_wall = 2.4;
+inside_wall = 1.2;
+use_inside_walls = true;
+inside_wall_height_reducer = 2;
+x_walls = width/2-1;
+y_walls = length/2-1;
+// x_walls = 1;
 
+// texture = undef;
 texture = "diamonds_vnf";
 tex_scale = 0.5;
+tex_size = [ BRICK_CALCULATE_PHYSICAL_LENGTH(2) / 2, BRICK_CALCULATE_PHYSICAL_HEIGHT(4) / 4 ];
+inside_walls_spacing = BIGBRICK_CALCULATE_PHYSICAL_LENGTH(2);
 
-tube_height = BRICK_CALCULATE_PHYSICAL_HEIGHT(height);
-tube_width = BRICK_CALCULATE_PHYSICAL_LENGTH(width);
-tube_length = BRICK_CALCULATE_PHYSICAL_LENGTH(length);
+pencil_organizer(width, length, height);
+
+
+
+module pencil_organizer(width, length, height)
+{
+
+
+tube_height = BIGBRICK_CALCULATE_PHYSICAL_HEIGHT(height);
+tube_width = BIGBRICK_CALCULATE_PHYSICAL_LENGTH(width);
+tube_length = BIGBRICK_CALCULATE_PHYSICAL_LENGTH(length);
 brick_height = 1 / 3;
-brick_physical_height = BRICK_CALCULATE_PHYSICAL_HEIGHT(brick_height);
-tube_wall = 2;
-brick_wall_thickness = BRICK_CALCULATE_PHYSICAL_WALL_THICKNESS();
+brick_physical_height = BIGBRICK_CALCULATE_PHYSICAL_HEIGHT(brick_height);
+
+brick_wall_thickness = BIGBRICK_CALCULATE_PHYSICAL_WALL_THICKNESS();
 echo("tube_wall", tube_wall, "tube_width", tube_width);
 
-tex_size = [ BRICK_CALCULATE_PHYSICAL_LENGTH(4) / 4, BRICK_CALCULATE_PHYSICAL_HEIGHT(4) / 4 ];
+
 
 // Tube with texture
 path = difference(rect([ tube_width, tube_length ]), rect([ tube_width - tube_wall * 2, tube_length - tube_wall * 2 ]));
@@ -34,13 +52,19 @@ diff()
   tag("remove") position(BOT)
     cuboid([ tube_width - brick_wall_thickness * 2, tube_length - brick_wall_thickness * 2, brick_physical_height ],
            anchor = BOT);
+  if (use_inside_walls)
+  {
+    position(BOT) xcopies(n=x_walls, spacing=inside_walls_spacing)cuboid([inside_wall, tube_length-tube_wall, tube_height-inside_wall_height_reducer], anchor=BOT);
+    position(BOT) ycopies(n=y_walls, spacing=inside_walls_spacing)cuboid([tube_width-tube_wall, inside_wall, tube_height-inside_wall_height_reducer], anchor=BOT);
+  } 
 }
 
 // The brick part
 difference()
 {
-  brick(width, length, brick_height, is_tile = true, texture = undef, tex_size = tex_size, tex_scale = tex_scale,
+  bigbrick(width, length, brick_height, is_tile = true, texture = undef, tex_size = tex_size, tex_scale = tex_scale,
         anchor = BOT);
   // Get rid of brick wall sticking out of tube wall's texture
-  rect_tube(h = brick_physical_height, size = [ tube_width, tube_length ], wall = 1);
+  rect_tube(h = brick_physical_height+1, size = [ tube_width, tube_length ], wall = 1);
+}
 }
